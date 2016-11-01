e5608125a2e021ac1a1436139cf2b8efaaa325e1
Niklas Gollenstede <n.gollenstede@web.de>
2016-11-01 17:08:51 +0100

    remove code for Chromium

--- background/index.js
+++ background/index.js
@@ -19,13 +19,6 @@ window.options = options;
 window.Profiles = Profiles;
 window.Chrome = window.Browser = arguments[0]['node_modules/web-ext-utils/chrome/'];
 
-let nativeConnector; // chrome only: NativeConnector instance while it runs, null while it is down
-let echoPortNum = 0; // chrome only: the port number the native echo server runs at
-let started = true; // may be set to a promise that must resolve for this module to load successfully
-const openMainFrameRequests = new Map; // tabId ==> Requests with .type === 'main_frame' that are currently active
-
-// start sync connection to content script
-if (gecko) {
 	Messages.addHandler('getSenderProfile', function() { // TODO: only allow for top frame
 		console.log('getSenderProfile', this);
 		const session = Profiles.getSessionForTab(this.tab.id, this.tab.url);
@@ -34,29 +27,6 @@ if (gecko) {
 		// for data: urls it should use the origin of the window.opener, if present, and be ignored otherwise
 		return session ? session.data : null;
 	});
-} else {
-	// in chrome the only way to send a synchronous message (from the content script to the background)
-	// is to set request headers to a sync XHR and have a local http server 'echo' those headers into the response
-
-	started = require.async('./native-connector').then(_ => new _({
-		version: 1,
-		ports: [ 46344, 35863, 34549, 40765, 48934, 47452, 10100, 5528 ],
-		onStart: async(function*() {
-			nativeConnector = this;
-			echoPortNum = (yield this.port.request('getPort'));
-			console.log(`Native app running on https://localhost:${ echoPortNum }/`);
-		}),
-		onStop() {
-			nativeConnector = null;
-			echoPortNum = 0;
-			console.error(`Native app closed, restarting ...`);
-			this.start();
-		},
-	}).start()).then(() => true);
-}
-
-// chrome only: this url (with the correct port) is used to load the options. But only while the main_frame is loading
-const getOptionsUrl = !gecko && (/^https\:\/\/localhost\:(\d+)\/stop_fingerprint_get_options$/);
 
 // TODO: do cached pages from the tab history pose a problem?
 // TODO: it seems that sync XHRs are not sent here by firefox
@@ -75,9 +45,6 @@ new RequestListener({
 
 		if (this.isMainFrame && gecko && url.startsWith('https://addons.mozilla.org/')) { return ignore; } // can't attach content_scripts anyway
 
-		this.isMainFrame && openMainFrameRequests.set(this.id, this);
-		this.isOptionsRequest = false; // chrome only
-
 		const session = this.session = this.isMainFrame
 		? Profiles.getSessionForPageLoad(tabId, url)
 		: Profiles.getSessionForTab(tabId, url);
@@ -90,41 +57,10 @@ new RequestListener({
 	}
 	destroy() {
 		// console.log('request end', this.requestId);
-		this.isMainFrame && openMainFrameRequests.delete(this.id);
-	}
-
-	// chrome only: filter requests to the echo server
-	[!gecko && 'onBeforeRequest']() {
-		const match = getOptionsUrl.exec(this.url);
-		if (!match) { return; }
-		if (+match[1] !== echoPortNum) { // cancel if port is wrong, the content script will try the next port
-			console.log('cancel getOptions request on wrong port', +match[1]);
-			return { cancel: true, };
-		}
-		if (false && !openMainFrameRequests.has(this.tabId)) { // cancel if the tabs main_frame isn't pending (to prevent the content from reading the options)
-			// TODO: this doesn't work yet
-			console.log(`cancel getOptions request made while the main_frame isn't pending`);
-			return { cancel: true, };
-		}
-		this.isOptionsRequest = true;
-	}
-
-	[!gecko && 'getEchoOptions']() {
-		if (!this.isOptionsRequest) { return null; }
-		return {
-			ignore,
-			requestHeaders: [
-				{ name: 'x-options', value: JSON.stringify(this.profile), },
-				{ name: 'x-nonce', value: this.profile.nonce, },
-			],
-		};
 	}
 
 	// TODO: (only?) firefox: this is not called for the favicon
 	onBeforeSendHeaders({ requestHeaders, }) {
-		const options = !gecko && this.getEchoOptions();
-		if (options) { return options; }
-
 		if (!this.session.navigator) { return; }
 
 		const order = this.session.navigator.headerOrder;
@@ -141,13 +77,8 @@ new RequestListener({
 	onHeadersReceived({ responseHeaders, }) {
 		let changed = false;
 
-		// not firefox: modify CSP to allow script injection
-		changed |= !gecko && (this.type === 'main_frame' || this.type === 'sub_frame') && responseHeaders.some(header => {
-			if ((/^(?:Content-Security-Policy)$/i).test(header.name) && header.value) { return this.injectCSP(header); }
-		});
-
-		// not firefox: disable HSTS header (has no effect in firefox)
-		changed |= !gecko && this.profile.hstsDisabled && responseHeaders.some(header => {
+		// not firefox: disable HSTS header (has no effect in firefox, but it should have, so leave the code for now)
+		changed |= this.profile.hstsDisabled && responseHeaders.some(header => {
 			if ((/^(?:Strict-Transport-Security)$/i).test(header.name) && header.value) { return this.removeHSTS(header); }
 		});
 
@@ -156,9 +87,6 @@ new RequestListener({
 		if (changed) { return { responseHeaders, }; }
 	}
 
-	onAuthRequired() {
-		if (this.type === 'main_frame') { this.session.detachFromTab(this.tabId); return reset; }
-	}
 	onBeforeRedirect() {
 		if (this.type === 'main_frame') { this.session.detachFromTab(this.tabId); return reset; }
 	}
@@ -203,72 +131,11 @@ new RequestListener({
 		headers['Connection'] = { name: 'Connection', value: 'keep-alive', };
 	}
 
-	injectCSP(header) {
-		let changed = false;
-		// modify CSPs to allow script injection
-		// reference: https://www.w3.org/TR/CSP/
-		// even though frame-src is deprecated, it will probably not be removed for a long time, and it provides a convenient way to allow 'blob:' only in workers
-		let defaultSrc = [ '*', ], scriptSrc, childSrc, frameSrc, others = [ ];
-		header.value.trim().split(/\s*;\s*/g).forEach(directive => { // TODO: check case sensitivity
-			if ((/^default-src\s+/i).test(directive)) { return (defaultSrc = directive.split(/\s+/g).slice(1)); }
-			if ((/^script-src\s+/i).test(directive)) { return (scriptSrc = directive.split(/\s+/g).slice(1)); }
-			if ((/^child-src\s+/i).test(directive)) { return (childSrc = directive.split(/\s+/g).slice(1)); }
-			if ((/^frame-src\s+/i).test(directive)) { return (frameSrc = directive.split(/\s+/g).slice(1)); }
-			others.push(directive);
-		});
-		!scriptSrc && (scriptSrc = defaultSrc.slice());
-		!childSrc && (childSrc = defaultSrc.slice());
-		!frameSrc && (frameSrc = childSrc.slice());
-
-		function inject(tokens, token, test = _=>_=== token) {
-			if (tokens.includes("'none'")) {
-				tokens.splice(0, Infinity, token);
-				return (changed = true);
-			}
-			if (!tokens.some(test)) {
-				tokens.unshift(token);
-				return (changed = true);
-			}
-		}
-
-		inject(scriptSrc, "'unsafe-eval'") && (this.profile.misc.disableEval = true);
-		inject(scriptSrc, `'nonce-${ this.profile.nonce }'`, $=>$ === "'unsafe-inline'");
-		inject(childSrc, 'blob:') && (this.profile.misc.disableChildBlobUrl = true);
-
-		if (!changed) { return; }
-		header.value
-		= 'default-src '+ defaultSrc.join(' ') +'; '
-		+ 'script-src '+ scriptSrc.join(' ') +'; '
-		+ 'child-src '+ childSrc.join(' ') +'; '
-		+ 'frame-src '+ frameSrc.join(' ') +'; '
-		+ others.join('; ');
-		// console.log('build CSP\n', header.value);
-		return true;
-	}
-
 	removeHSTS(header) {
 		header.value = header.value.replace(/max-age=\d+/gi, () => 'max-age=0');
 		// console.log('HSTS header removed', this.domain, header);
 		return true;
 	}
-});
-
-// chrome only: clear cache on requests
-let clearCacheWhat = null, clearCacheWhere = null;
-const clearCache = throttle(() => browsingData.remove({ since: 0, originTypes: clearCacheWhere, }, clearCacheWhat), 3000);
-options.children.clearCache.children.what.whenChange(value => clearCacheWhat = ({
-	passive:  { appcache: true, cache: true, pluginData: true, },
-	active:   { appcache: true, cache: true, pluginData: true, serviceWorkers: true, cookies: true, serverBoundCertificates: true, indexedDB: true, localStorage: true, webSQL: true, fileSystems: true, },
-	all:      { appcache: true, cache: true, pluginData: true, serviceWorkers: true, cookies: true, serverBoundCertificates: true, indexedDB: true, localStorage: true, webSQL: true, fileSystems: true, downloads: true, formData: true, history: true, passwords: true, },
-})[value]);
-options.children.clearCache.children.where.whenChange(value => clearCacheWhere = ({
-	unprotectedWeb:  { unprotectedWeb: true, },
-	protectedWeb:    { unprotectedWeb: true, protectedWeb: true, },
-	extension:       { unprotectedWeb: true, protectedWeb: true, extension: true, },
-})[value]);
-options.children.clearCache.children.where.when({
-	false: () => webRequest.onHeadersReceived.removeListener(clearCache), // is always false in firefox
-	true: () => webRequest.onHeadersReceived.addListener(clearCache, { urls: [ '*://*/*', ], }, [ ]),
 });
 
 // let other views and the content post notifications
@@ -295,7 +162,5 @@ Tabs.onUpdated.addListener(function(tabId, info, { url, }) {
 		: { tabId, icon: 'temp', title: 'tempActive', }
 	);
 });
-
-(yield started);
 
 }); })();
--- common/options.js
+++ common/options.js
@@ -7,46 +7,6 @@
 }) {
 
 const model = deepFreeze({
-	clearCache: Object.assign({
-		title: 'Disable Caching',
-		type: 'label',
-	}, blink ? { // chrome
-		description: `In Chrome caching can't actually be disabled. The best that is possible (without command line flags) is to clear the browsing data on every web request`,
-		default: true,
-		children: [
-			{
-				name: 'where',
-				title: 'Where',
-				description: `Choose the type of locations for which to delete the browsing data`,
-				type: 'menulist',
-				default: 'unprotectedWeb',
-				options: [
-					{ value: false, label: `Nowhere. Don't interfere with the caching or any other browsing data (not recommended)`, },
-					{ value: 'unprotectedWeb', label: `Websites that are not explicitly marked as protected (recommended)`, },
-					{ value: 'protectedWeb', label: `Also include websites that are marked as "protected" by other extensions or chrome apps`, },
-					{ value: 'extension', label: `Even include extension data. It is probably not a good idea to select this`, },
-				],
-			}, {
-				name: 'what',
-				title: 'What',
-				description: `Choose which type of browsing data to delete`,
-				type: 'menulist',
-				default: 'active',
-				options: [
-					{ value: 'passive', label: `Passive server side cache only: Cache, AppCache and Plugin Data`, },
-					{ value: 'active', label: `Also include programmatic storage: Cookies, JavaScript storages and Service Workers (recommended)`, },
-					{ value: 'all', label: `All browsing data: Complete History including Downloads, Form Data and Saved Passwords`, },
-				],
-			},
-		],
-	} : { // firefox
-		description: `This extension can't disable caching in Firefox. See the Firefox tab for more information`,
-		default: false,
-		children: [
-			{ name: 'where', type: 'hidden', default: false, },
-			{ name: 'what', type: 'hidden', default: false, },
-		],
-	}),
 	debug: {
 		title: 'Enable debugging',
 		description: `Enable some stuff that can definitely be used to compromise your privacy and security but is helpful when debugging`,
